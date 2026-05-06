304cc327a716529bb1446662c88cf77315388f2c
abdulazizizisan <binabdulaziz4983@gmail.com>
2026-05-06 17:08:26 +0600

    Add interactive menu and banner to main application

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,54 @@
+use std::io::{stdin, stdout, Write};
+
 fn main() {
-    println!("Hello, world!");
+    print_banner();
+    let mut user_input = String::new();
+    loop {
+        print_menu();
+        stdout().flush().unwrap();
+
+        match stdin().read_line(&mut user_input) {
+            Ok(_) => {}
+            Err(_) => {
+                println!("Something went wrong. Please enter again.");
+                continue
+            }
+        }
+
+        let user_input: i8 = match user_input.trim().parse() {
+            Ok(val) => val,
+            Err(_) => {
+                println!("Please enter a valid number.");
+                continue;
+            }
+        };
+
+        if user_input == 0 {
+            break;
+        }
+    }
+}
+
+fn print_menu() {
+    let menu = "
+    1. Add Item
+    2. View All Items
+    3. Search Item
+    4. Update Quantity
+    5. Remove Item
+    6. View Summary
+    0. Quit
+
+    Choose an option: ";
+
+    print!("{menu}");
+}
+
+fn print_banner() {
+    let banner = "
+    ╔═╗┌─┐┌─┐┬┌─┌─┐┌┬┐  ╔═╗┌─┐┌┐┌┌┬┐┬─┐┬ ┬  ╔═╗╦  ╦
+    ╠═╝│ ││  ├┴┐├┤  │   ╠═╝├─┤│││ │ ├┬┘└┬┘  ║  ║  ║
+    ╩  └─┘└─┘┴ ┴└─┘ ┴   ╩  ┴ ┴┘└┘ ┴ ┴└─ ┴   ╚═╝╩═╝╩";
+
+    print!("{banner}");
 }
